9e4579c232423b87b486cf45145ad3ae3b158b18
phung1003 <anhungtrnc@gmail.com>
2024-11-20 17:59:50 +0700

    1

--- MM-src/src/main/java/module-info.java
+++ MM-src/src/main/java/module-info.java
@@ -3,9 +3,9 @@ module com.example.mm {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
-    requires java.desktop;
     requires okhttp3;
     requires org.json;
+    requires java.desktop;
 
     opens com.example.mm to javafx.fxml;
     exports com.example.mm;
